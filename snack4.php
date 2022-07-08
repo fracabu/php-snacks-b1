e5b1f9ef0b12e974f145c038e991fbebbbbdfe2a
fracabu <100199965+fracabu@users.noreply.github.com>
2022-07-08 18:26:15 +0200

    mod

---
+++ snack4.php
@@ -0,0 +1,12 @@
+<!-- ## Snack 4
+Creare un array con 15 numeri casuali, tenendo conto che l’array
+non dovrà contenere lo stesso numero più di una volta -->
+
+<?php
+$random=array();
+for($i=0; $i<10; $i++)
+{
+$random[]=rand(0,100)."<br />";
+}
+print_r($random);
+?>
